d71a8c1555fdcbcb4601fb237ac30e759560ebe0
swarnimky <swarnimky88@gmail.com>
2022-03-28 22:01:27 +0530

    done

---
+++ todo-app/src/App.js
@@ -0,0 +1,59 @@
+import './App.css';
+import { Fragment, useEffect ,useState } from 'react';
+import TodoItem from './components/todoItem';
+
+function App() {
+   
+  const[todoItems ,setTodoItems] = useState(null);
+
+  useEffect(() => {
+    //do something on load
+    console.log("Hey I loaded up");
+  
+    if(!todoItems){
+      fetch("http://localhost:8080/api/todoItems")
+    .then((response) => response.json())
+    .then((data)  => {
+      console.log("Todo items list : " ,data);
+      setTodoItems(data);
+    }); 
+  }   
+   } , [todoItems]);
+
+   function addNewTodoItem(){
+    fetch("http://localhost:8080/api/todoItems",{
+      headers: {
+        "content-type": "application/json",
+    },
+           method : "post",
+         
+         }) 
+         .then((response) => response.json())
+         .then((aTodoItem) => {
+  
+          setTodoItems([...todoItems , aTodoItem]);
+           });
+   }
+  function handleDeleteTodoItem(item){
+ const updatedTodoItems =  todoItems.filter(aTodoItem => aTodoItem.id !==item.id);
+ console.log('Updated todo items' , updatedTodoItems);  
+ setTodoItems([...updatedTodoItems]);
+  }
+  return ( 
+    <>
+    <div>
+     <button onClick={addNewTodoItem}> Add new Item</button>
+</div>
+<div>
+  {
+   todoItems ? todoItems.map((todoItem) => {
+  return <TodoItem key={todoItem.id} data={todoItem} emitDeleteTodoItem={handleDeleteTodoItem} />;
+  }) : 'loading data...'}
+ </div>
+    </>
+    
+    
+  );
+}
+
+export default App;
---
+++ todo-app/src/components/todoItem.jsx
@@ -0,0 +1,62 @@
+import React, { useState , useEffect} from 'react';
+
+const TodoItem = (props) => {
+    const {emitDeleteTodoItem} =props;
+  const [todoItem , setTodoItem] = useState(props.data);
+  const[isDirty ,setDirty] =useState(false); 
+ useEffect(() => {
+     if(isDirty) {
+        fetch(`http://localhost:8080/api/todoItems/${todoItem.id}` , {
+            method : "put",
+            
+            headers: {
+                "content-type" : " application/json",
+            },
+            body: JSON.stringify(todoItem),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            setDirty(false);
+            setTodoItem(data);
+        });
+     }
+ }, [todoItem, isDirty])
+
+
+  function updateTask (e){
+      setDirty(true);
+      setTodoItem({...todoItem ,task : e.target.value});
+      
+    }
+    function deleteTodoItem(){
+        fetch(`http://localhost:8080/api/todoItems/${todoItem.id}` , {
+            method : "DELETE",   
+            headers: {
+                "content-type" : " application/json",
+            },
+            body: JSON.stringify(todoItem),
+        })
+        .then((response) =>{
+            
+            emitDeleteTodoItem(todoItem);
+        })
+       
+    }
+    return (
+        <div> 
+        <input type="checkbox" checked={todoItem.isDone}
+        onChange= {() =>  {
+            setDirty(true); 
+            setTodoItem({...todoItem , isDone : !todoItem.isDone});
+        }}
+    
+         /> 
+         {
+           todoItem.isDone ? <span style={{textDecoration:"line-through"}} >{todoItem.task}</span> : <input type ="text" value ={todoItem.task}  onChange={updateTask} />
+         }
+       <span style={{marginLeft : "2rem" , cursor:"pointer"}} onClick={deleteTodoItem} >🗑️</span>
+       </div>
+    );
+};
+
+export default TodoItem;
