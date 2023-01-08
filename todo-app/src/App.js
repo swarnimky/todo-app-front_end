5814cb073517a43cbdc79f51f47f21fb10a03bde
swarnimyadav <swarnim.yadav@rksv.in>
2023-01-08 21:00:55 +0530

    netlify start

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,58 +1,62 @@
-import './App.css';
-import { Fragment, useEffect ,useState } from 'react';
-import TodoItem from './components/todoItem';
+import "./App.css";
+import { Fragment, useEffect, useState } from "react";
+import TodoItem from "./components/todoItem";
 
 function App() {
-   
-  const[todoItems ,setTodoItems] = useState(null);
+  const [todoItems, setTodoItems] = useState(null);
 
   useEffect(() => {
     //do something on load
     console.log("Hey I loaded up");
-  
-    if(!todoItems){
-      fetch("http://localhost:8080/api/todoItems")
-    .then((response) => response.json())
-    .then((data)  => {
-      console.log("Todo items list : " ,data);
-      setTodoItems(data);
-    }); 
-  }   
-   } , [todoItems]);
 
-   function addNewTodoItem(){
-    fetch("http://localhost:8080/api/todoItems",{
+    if (!todoItems) {
+      fetch("http://localhost:8080/api/todoItems")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Todo items list : ", data);
+          setTodoItems(data);
+        });
+    }
+  }, [todoItems]);
+
+  function addNewTodoItem() {
+    fetch("http://localhost:8080/api/todoItems", {
       headers: {
         "content-type": "application/json",
-    },
-           method : "post",
-         
-         }) 
-         .then((response) => response.json())
-         .then((aTodoItem) => {
-  
-          setTodoItems([...todoItems , aTodoItem]);
-           });
-   }
-  function handleDeleteTodoItem(item){
- const updatedTodoItems =  todoItems.filter(aTodoItem => aTodoItem.id !==item.id);
- console.log('Updated todo items' , updatedTodoItems);  
- setTodoItems([...updatedTodoItems]);
+      },
+      method: "post",
+    })
+      .then((response) => response.json())
+      .then((aTodoItem) => {
+        setTodoItems([...todoItems, aTodoItem]);
+      });
   }
-  return ( 
+  function handleDeleteTodoItem(item) {
+    const updatedTodoItems = todoItems.filter(
+      (aTodoItem) => aTodoItem.id !== item.id
+    );
+    console.log("Updated todo items", updatedTodoItems);
+    setTodoItems([...updatedTodoItems]);
+  }
+  return (
     <>
-    <div>
-     <button onClick={addNewTodoItem}> Add new Item</button>
-</div>
-<div>
-  {
-   todoItems ? todoItems.map((todoItem) => {
-  return <TodoItem key={todoItem.id} data={todoItem} emitDeleteTodoItem={handleDeleteTodoItem} />;
-  }) : 'loading data...'}
- </div>
+      <div>
+        <button onClick={addNewTodoItem}> Add new Item</button>
+      </div>
+      <div>
+        {todoItems
+          ? todoItems.map((todoItem) => {
+              return (
+                <TodoItem
+                  key={todoItem.id}
+                  data={todoItem}
+                  emitDeleteTodoItem={handleDeleteTodoItem}
+                />
+              );
+            })
+          : "loading data from the server..."}
+      </div>
     </>
-    
-    
   );
 }
 
